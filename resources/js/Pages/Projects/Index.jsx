991a5d04a69b429f367def3396b024e47332f7e8
Jonathan Buso <jonathanbuso.26@gmail.com>
2024-03-11 03:28:04 +0100

    updated projects page design

--- resources/js/Pages/Projects/Index.jsx
+++ resources/js/Pages/Projects/Index.jsx
@@ -22,32 +22,100 @@ export default function Index({ auth, projects }) {
         >
             <Head title="Projects" />
 
-            <div className="container max-w-7xl mx-auto py-8">
-                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-                    {
-                        projects.map(project =>
-                            <Project key={project.id} project={project} />
-                        )
-                    }
-                    {/* Project Card */}
-                    <div className="bg-white rounded-lg shadow-md p-6">
-                        <h2 className="text-xl font-semibold mb-2">Project 1</h2>
-                        <p className="text-gray-600 mb-4">Description of Project 1</p>
-                        <a href="#" className="text-blue-500 hover:underline">View Details</a>
+            <div className="container max-w-7xl mx-auto">
+                {
+                    projects.map(project =>
+                        <Project key={project.id} project={project} />
+                    )
+                }
+                <div className='py-8'>
+                    <div className='px-4'>
+                        <div className='flex items-center justify-between'>
+                            <h2 className="text-sm font-bold">Pinned Projects</h2>
+                            <button type="button" className="flex-none h-9 items-center justify-center bg-green-500 rounded-md text-white hover:text-gray-200 focus:outline-none">
+                                <span className="px-4">New Project +</span>
+                            </button>
+                        </div>
+                        <ul className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 mt-3">
+                            <li className="flex rounded-md">
+                                <div className="text-sm bg-pink-600 w-16 rounded-l-md flex items-center justify-center text-white font-bold">GA</div>
+                                <div className="text-sm flex flex-1 items-center rounded-r-md border-t border-b border-r">
+                                    <div className="flex-1 px-4 py-2">
+                                        <a href="#" className="text-gray-200 hover:text-gray-600 font-semibold">Graph API</a>
+                                        <p className="text-gray-500">16 Members</p>
+                                    </div>
+                                    <div>
+                                        <button type="button" className="flex h-8 w-8 items-center justify-center text-gray-700 hover:text-gray-900 focus:outline-none">
+                                            <span className="absolute w-1 h-1 p-0 m-1 overflow-hidden clip whitespace-nowrap border-0">Open options</span>
+                                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5 inline-block">
+                                                <path d="M10 3a1.5 1.5 0 110 3 1.5 1.5 0 010-3zM10 8.5a1.5 1.5 0 110 3 1.5 1.5 0 010-3zM11.5 15.5a1.5 1.5 0 10-3 0 1.5 1.5 0 003 0z"></path>
+                                            </svg>
+                                        </button>
+                                    </div>
+                                </div>
+                            </li>
+                            <li className="flex rounded-md">
+                                <div className="text-sm bg-yellow-400 w-16 rounded-l-md flex items-center justify-center text-white font-bold">CD</div>
+                                <div className="text-sm flex flex-1 items-center rounded-r-md border-t border-b border-r">
+                                    <div className="flex-1 px-4 py-2">
+                                        <a href="#" className="text-gray-200 hover:text-gray-600 font-semibold">Component Design</a>
+                                        <p className="text-gray-500">12 Members</p>
+                                    </div>
+                                    <div>
+                                        <button type="button" className="flex h-8 w-8 items-center justify-center text-gray-700 hover:text-gray-900 focus:outline-none">
+                                            <span className="absolute w-1 h-1 p-0 m-1 overflow-hidden clip whitespace-nowrap border-0">Open options</span>
+                                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-4 h-4 inline-block">
+                                                <path d="M10 3a1.5 1.5 0 110 3 1.5 1.5 0 010-3zM10 8.5a1.5 1.5 0 110 3 1.5 1.5 0 010-3zM11.5 15.5a1.5 1.5 0 10-3 0 1.5 1.5 0 003 0z"></path>
+                                            </svg>
+                                        </button>
+                                    </div>
+                                </div>
+                            </li>
+                        </ul>
                     </div>
-                    {/* Project Card */}
-                    <div className="bg-white rounded-lg shadow-md p-6">
-                        <h2 className="text-xl font-semibold mb-2">Project 2</h2>
-                        <p className="text-gray-600 mb-4">Description of Project 2</p>
-                        <a href="#" className="text-blue-500 hover:underline">View Details</a>
+                </div>
+                <div className='py-8'>
+                    <div className='px-4'>
+                        <div className='flex items-center justify-between'>
+                            <h2 className="text-sm font-bold">All Projects</h2>
+                        </div>
+                        <ul className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 mt-3">
+                            <li className="flex rounded-md">
+                                <div className="text-sm bg-orange-300 w-16 rounded-l-md flex items-center justify-center text-white font-bold">P1</div>
+                                <div className="text-sm flex flex-1 items-center rounded-r-md border-t border-b border-r">
+                                    <div className="flex-1 px-4 py-2">
+                                        <a href="#" className="text-gray-200 hover:text-gray-600 font-semibold">Project 1</a>
+                                        <p className="text-gray-500">2 Members</p>
+                                    </div>
+                                    <div>
+                                        <button type="button" className="flex h-8 w-8 items-center justify-center text-gray-700 hover:text-gray-900 focus:outline-none">
+                                            <span className="absolute w-1 h-1 p-0 m-1 overflow-hidden clip whitespace-nowrap border-0">Open options</span>
+                                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5 inline-block">
+                                                <path d="M10 3a1.5 1.5 0 110 3 1.5 1.5 0 010-3zM10 8.5a1.5 1.5 0 110 3 1.5 1.5 0 010-3zM11.5 15.5a1.5 1.5 0 10-3 0 1.5 1.5 0 003 0z"></path>
+                                            </svg>
+                                        </button>
+                                    </div>
+                                </div>
+                            </li>
+                            <li className="flex rounded-md">
+                                <div className="text-sm bg-blue-600 w-16 rounded-l-md flex items-center justify-center text-white font-bold">P2</div>
+                                <div className="text-sm flex flex-1 items-center rounded-r-md border-t border-b border-r">
+                                    <div className="flex-1 px-4 py-2">
+                                        <a href="#" className="text-gray-200 hover:text-gray-600 font-semibold">Project 2</a>
+                                        <p className="text-gray-500">6 Members</p>
+                                    </div>
+                                    <div>
+                                        <button type="button" className="flex h-8 w-8 items-center justify-center text-gray-700 hover:text-gray-900 focus:outline-none">
+                                            <span className="absolute w-1 h-1 p-0 m-1 overflow-hidden clip whitespace-nowrap border-0">Open options</span>
+                                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-4 h-4 inline-block">
+                                                <path d="M10 3a1.5 1.5 0 110 3 1.5 1.5 0 010-3zM10 8.5a1.5 1.5 0 110 3 1.5 1.5 0 010-3zM11.5 15.5a1.5 1.5 0 10-3 0 1.5 1.5 0 003 0z"></path>
+                                            </svg>
+                                        </button>
+                                    </div>
+                                </div>
+                            </li>
+                        </ul>
                     </div>
-                    {/* Project Card */}
-                    <div className="bg-white rounded-lg shadow-md p-6">
-                        <h2 className="text-xl font-semibold mb-2">Project 3</h2>
-                        <p className="text-gray-600 mb-4">Description of Project 3</p>
-                        <a href="#" className="text-blue-500 hover:underline">View Details</a>
-                    </div>
-                    {/* Add more project cards as needed */}
                 </div>
             </div>
         </AuthenticatedLayout>
